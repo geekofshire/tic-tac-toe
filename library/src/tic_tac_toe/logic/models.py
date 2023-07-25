68bf9fd603377323abad3eb002b62bd15f2734f6
Rohan <nickeyjha2@gmail.com>
2023-07-25 19:46:59 +0530

    added models

--- library/src/tic_tac_toe/logic/models.py
+++ library/src/tic_tac_toe/logic/models.py
@@ -1,8 +1,9 @@
-
 import re
 import enum
 from dataclasses import dataclass
 from functools import cached_property
+from tic_tac_toe.logic.validators import validate_game_state, validate_grid
+from tic_tac_toe.logic.exceptions import InvalidMove
 
 
 WINNING_PATTERNS = (
@@ -29,8 +30,7 @@ class Grid:
     cells: str = " " * 9
 
     def __post_init__(self) -> None:
-        if not re.match(r"^[\sXO]{9}$", self.cells):
-            raise ValueError("Must contain 9 cells of: X, O, or space")
+        validate_game_state(self)
 
     @cached_property
     def x_count(self) -> int:
@@ -94,4 +94,29 @@ class GameState:
                         for match in re.finditer(r"\?", pattern)
                     ]
         return []
+    
+    @cached_property
+    def possible_moves(self) -> list[Move]:
+        moves = []
+        if not self.game_over:
+            for match in re.finditer(r"\s", self.grid.cells):
+                moves.append(self.make_move_to(match.start()))
+        return moves
+    
+    def make_move_to(self, index: int) -> Move:
+        if self.grid.cells[index] != " ":
+            raise InvalidMove("Cell is not empty")
+        return Move(
+            mark=self.current_mark,
+            cell_index=index,
+            before_state=self,
+            after_state=GameState(
+                Grid(
+                    self.grid.cells[:index]
+                    + self.current_mark
+                    + self.grid.cells[index + 1:]
+                ),
+                self.starting_mark,
+            ),
+        )
     
